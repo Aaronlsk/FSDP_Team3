081d897bab6e95ec531908be0803442f323582fe
Aaron <S10258287@connect.np.edu.sg>
2025-01-25 16:21:37 +0800

    region change

--- fsdp/src/aws-config.js
+++ fsdp/src/aws-config.js
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk';
 
-// Configure AWS SDK
+// Configure AWS SDK //
 AWS.config.update({
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
